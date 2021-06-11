0c95ca49b10fca36779e0f3522260c84937c59cd
jenvigo <jenvigo@gmail.com>
2021-06-12 01:24:10 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$url = 'https://datosmacro.expansion.com/paro/espana';
+$d = new DOMDocument;
+$d->loadHTML(file_get_contents($url));
+$finder = new DomXPath($d);
+//*[@id="tb0"]/tbody/tr[1]/td[2]
+///html/body/div[3]/div[1]/div/section/div/article/div/div[3]/div[1]/div[2]/table/tbody/tr[1]/td[2]
+$classname="tabledat";
+$nodes = $finder->query('//*[@id="tb0"]/tbody/tr[1]/td[2]');
+?>
+<?php
+//var_dump($nodes);
+//var_dump($nodes->item(0)->nodeValue);
+$paro = ($nodes->item(0)->nodeValue);
+?>
+
